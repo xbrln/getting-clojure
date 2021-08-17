5a44c99ca20ced89b0b73994e973d0fd42d4f7d7
jeffy <j.kurisingal@holidaypirates.com>
2021-08-17 22:01:15 +0200

    Fibonacci

--- src/getting_clojure/intermediate/fizzbuzz.clj
+++ src/getting_clojure/intermediate/fizzbuzz.clj
@@ -6,47 +6,44 @@
 ; For multiples of 5, print Buzz
 ; For multiples of both 3 & 5, print FizzBuzz
 
-(defn multiple-of? [d n]
+(defn multiple-of?
+  "Predicate to check if a number is divisible by divisor"
+  [d n]
   (zero? (mod n d)))
 
-(defn fizzbuzz [n]
+(defn fizzbuzz
+  "FizzBuzz implementation using mod"
+  [n]
   (cond
-    (multiple-of? (* 3 5) n) "FizzBuzz"
+    (multiple-of? 15 n) "FizzBuzz"
     (multiple-of? 5 n) "Buzz"
     (multiple-of? 3 n) "Fizz"
     :else n))
 
 (take 100 (map fizzbuzz (rest (range))))
 
-(comment
-  ; With condp
-  (for [n (range 1 101)]
-    (condp multiple-of? n
-      (* 3 5) "FizzBuzz"
-      5 "Buzz"
-      3 "Fizz"
-      n))
+; With condp
+(for [n (range 1 101)]
+  (condp multiple-of? n
+    15 "FizzBuzz"
+    5 "Buzz"
+    3 "Fizz"
+    n))
 
-  ; Without cond, using cycle
-  (defn fb
-    [a b]
-    (if (s/blank? a)
-      b
-      a))
-
-  (take 100
-    (map fb
-      (map str
-        (cycle [nil nil "Fizz"])
-        (cycle [nil nil nil nil "Buzz"]))
-      (rest (range))))
-
-  ; with cycle and or
-  (map
-    (fn [a b c d] (or a b c d))
-    (cycle [nil nil nil nil nil
-            nil nil nil nil nil
-            nil nil nil nil "FizzBuzz"])
+; Without cond, using cycle
+(map
+  (fn [a b](if (s/blank? a) b a))
+  (map str
     (cycle [nil nil "Fizz"])
-    (cycle [nil nil nil nil "Buzz"])
-    (range 1 101)))
+    (cycle [nil nil nil nil "Buzz"]))
+  (range 1 101))
+
+; With cycle, with or
+(map
+  (fn [a b c d] (or a b c d))
+  (cycle [nil nil nil nil nil
+          nil nil nil nil nil
+          nil nil nil nil "FizzBuzz"])
+  (cycle [nil nil "Fizz"])
+  (cycle [nil nil nil nil "Buzz"])
+  (range 1 101))
